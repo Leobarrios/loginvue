598aa4ac559243b3b92a7545dc60d9a7fd3511b5
Leonardo Barrios <leobarrios_23@hotmail.com>
2019-07-17 19:05:13 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Login></Login>
+    
+  </div>
+</template>
+
+<script>
+import Login from "./components/Login.vue";
+
+export default {
+  name: "app",
+  components: {
+    Login,
+  }
+};
+</script>
+
---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Ingrese Email</label>
+    <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Contraseña</label>
+    <input v-model="pass" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <button v-on:click="login" type="submit" class="btn btn-primary">Entrar</button>
+</form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return{
+            email:'',
+            pass:'',
+            usuarios:[],
+        }
+    },
+
+mounted() {
+    this.loadUsers(),
+    this.login()
+
+},
+methods: {
+    loadUsers: async function() {
+        const data = await fetch('./users.json')
+        this.usuarios = await data.json()
+    },
+    login(){
+       if (this.usuarios.email == this.email && this.usuarios.pass == this.pass) {
+           console.log("todo piola amigo")
+       }
+       else{
+           console.log("nada piola amigo")
+       } 
+    },
+  }
+}
+</script>
+
